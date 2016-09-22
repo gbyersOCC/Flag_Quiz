a418bc66684b7609c114a1684cd039f49aba2d88
gbyers <gbyers@student.cccd.edu>
2016-09-22 16:27:28 -0700

    Only finished with step 2 on pdf pics

--- app/src/main/java/edu/orangecoastcollege/cs273/gbyers/flagquiz/QuizActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/gbyers/flagquiz/QuizActivity.java
@@ -1,15 +1,20 @@
 package edu.orangecoastcollege.cs273.gbyers.flagquiz;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class QuizActivity extends AppCompatActivity {
+
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGION = "pref_regions";
+
+     private boolean phoneDevice = true;
+    private boolean preferencesChanged = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +23,8 @@ public class QuizActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
     }
 
     @Override
