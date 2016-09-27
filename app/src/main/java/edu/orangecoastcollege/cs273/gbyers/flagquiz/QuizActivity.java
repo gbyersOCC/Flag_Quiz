c93fc4d90a50174f3b1a4a42725fbf51b57a28cc
grant <gbyersOCC>
2016-09-27 10:34:35 -0700

    HALF DONE

--- app/src/main/java/edu/orangecoastcollege/cs273/gbyers/flagquiz/QuizActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/gbyers/flagquiz/QuizActivity.java
@@ -1,18 +1,25 @@
 package edu.orangecoastcollege.cs273.gbyers.flagquiz;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class QuizActivity extends AppCompatActivity {
 
     public static final String CHOICES = "pref_numberOfChoices";
-    public static final String REGION = "pref_regions";
+    public static final String REGIONS = "pref_regions";
 
-     private boolean phoneDevice = true;
+    private boolean phoneDevice = true;
     private boolean preferencesChanged = true;
 
 
@@ -25,27 +32,123 @@ public class QuizActivity extends AppCompatActivity {
 
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(preferencesChangeListener);
+
+        //determine screen size
+        int screenSize = getResources()
+                .getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        //if device is a tablet, set phoneDevice to false
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE || screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+            phoneDevice = false;
+
+        //if runnnig on phone sized device, allow only portrait orientation
+        if (phoneDevice)
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
     }
 
+    /**
+     * onStart is called after onCreate completes execution
+     * methof updates the number of guess rows to display and the regiond to choose flags from,
+     * then resets the quiz with new preferences
+     */
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        if(preferencesChanged)
+        {
+            //now that the default preferences have been set
+            //initialize quizActivity and start the quiz
+            QuizActivityFragment quizFragment = (QuizActivityFragment)
+                    getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+            quizFragment.updateGuessRows(PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.updateRegions(PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.resetQuiz();
+            preferencesChanged = false;
+        }
+    }
+
+    /**
+     * Shows Settings Menu ONLY if used by Phone or Portrait-orientation tablet
+     * @param menu  the settings menu
+     * @return true if Settings View was inflated, false otherwise
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_quiz, menu);
+    //get devices current orientation
+        int orientation = getResources().getConfiguration().orientation;
+
+        //display apps setngs menu only in portrait orientation
+        if (orientation == Configuration.ORIENTATION_PORTRAIT){
+            //inflate the menu
+        getMenuInflater().inflate(R.menu.menu_quiz,menu);
         return true;
+        }
+        else
+            return false;
     }
 
+    /**
+     * displays the SettingsActivity when running on phone or Portrait-oriented tablet.
+     * Starts the activity by use of Intent(Noo data passed, because the shared preferences, preferences.xml,has all data necessary)
+     * @param item the menu item
+     * @return  true, if an optionItem was selected
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Intent preferenceIntent = new Intent(this, SettingsActivity.class);
+
+        startActivity(preferenceIntent);
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Listener to handlle the Preference changes in the apps shared preferences (preferences.xml)
+     * If either regions or guess (options) have changed the quiz will restart with those new set settings
+     */
+    private SharedPreferences.OnSharedPreferenceChangeListener preferencesChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener()
+            //called when user changes apps preferences
+    {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPeezees, String key) {
+            preferencesChanged = true;
+
+            QuizActivityFragment quizFrag = (QuizActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+
+            if (key.equals(CHOICES))//# of coices to display has  to change
+            {
+                quizFrag.updateGuessRows(sharedPeezees);
+                quizFrag.resetQuiz();
+            } else if (key.equals(REGIONS))//Regions setting changed
+            {
+                Set<String> region = sharedPeezees.getStringSet(REGIONS, null);
+
+                if (region != null && region.size() > 0) {
+                    quizFrag.updateRegion(sharedPeezees);
+                    quizFrag.resetQuiz();
+                }
+                else    //must select one region (select North America by default)
+                {
+                    SharedPreferences.Editor editor = sharedPeezees.edit();
+                    region.add(getString(R.string.default_region));
+                    editor.putStringSet(REGIONS, region);
+                    editor.apply();
+
+                    Toast.makeText(QuizActivity.this,R.string.default_region_message, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        Toast.makeText(QuizActivity.this,R.string.restarting_quiz,Toast.LENGTH_SHORT)
+                .show();
+        }
+
+
+    };
 }
+
+
--- app/src/main/java/edu/orangecoastcollege/cs273/gbyers/flagquiz/QuizActivityFragment.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/gbyers/flagquiz/QuizActivityFragment.java
@@ -5,18 +5,80 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Handler;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class QuizActivityFragment extends Fragment {
 
+    private static final String TAG = "FlagQuiz Activity";
+    private static final int FLAGS_IN_QUIZ = 10;
+
+    private List<String> fileNameList; //flag file names
+    private List<String> quizCountriesList; //countries in current quiz
+    private Set<String> regionSet;
+    private String correctAnswer;
+    private int guessRows;
+    private int totalGuesses;
+    private int correctAnswers;
+    private SecureRandom random;
+    private Handler handler;
+
+    private TextView questionNumberTextView;
+    private ImageView flagImageView;
+    private LinearLayout[] guessLinearLayout;
+    private TextView answerTextView;
     public QuizActivityFragment() {
     }
 
+    /**
+     * configures the QuizActivity Fragment when its view is created
+     * @param inflater  the layout inflater
+     * @param container the ViewGroup where the fragment resides
+     * @param savedInstanceState
+     * @return
+     */
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_quiz, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+      View view =  inflater.inflate(R.layout.fragment_quiz, container, false);
+        fileNameList = new ArrayList<>();
+        quizCountriesList = new ArrayList<>();
+        random = new SecureRandom();
+        handler = new Handler();
+
+        //get ref to GUI componenets
+        questionNumberTextView = (TextView) view.findViewById(R.id.questionNumberTextView);
+        flagImageView = (ImageView) view.findViewById(R.id.flagImageView);
+        guessLinearLayout = new LinearLayout[4];
+        guessLinearLayout[0] = (LinearLayout) view.findViewById(R.id.rowOneLinearLayout);
+        guessLinearLayout[1] = (LinearLayout) view.findViewById(R.id.rowTwoLinearLayout);
+        guessLinearLayout[2] = (LinearLayout) view.findViewById(R.id.rowThreeLinearLayout);
+       // guessLinearLayout[3] = (LinearLayout) view.findViewById(R.id.rowFourLinearLayout);
+        answerTextView = (TextView) view.findViewById(R.id.answerTextView);
+        for(LinearLayout row: guessLinearLayout)
+        {
+            for(int column = 0; column < row.getChildCount(); column++)
+            {
+                Button button = (Button) row.getChildAt(column);
+                button.setOnClickListener(guessButtonListener);
+            }
+        }
+        //set questionNumberTextViews Text
+        questionNumberTextView.setText(getString(R.string.question, 1, FLAGS_IN_QUIZ));
+        return view; // return fragments view for display
     }
 }
